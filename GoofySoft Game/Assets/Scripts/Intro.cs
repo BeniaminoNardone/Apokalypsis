685501c2088d14b001e669f0f7825bb39ac497e9
Daddyna01 <dsenese23@fed.idserve.net>
2024-02-27 12:55:34 +0100

    Adidu
    Ho aggiunto 2 song

--- GoofySoft Game/Assets/Scripts/Intro.cs
+++ GoofySoft Game/Assets/Scripts/Intro.cs
@@ -6,13 +6,13 @@ public class Intro : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-       if(!PlayerPrefs.HasKey("firstTime") || PlayerPrefs.GetInt("firstTime")!= 69)
+       if(!PlayerPrefs.HasKey("firstTime") || PlayerPrefs.GetInt("firstTime")!= 70)
         {
-            PlayerPrefs.SetInt("firstTime", 69);
+            PlayerPrefs.SetInt("firstTime", 70);
             PlayerPrefs.Save();
 
         }
-       else if(PlayerPrefs.GetInt("firstTime") == 69)
+       else if(PlayerPrefs.GetInt("firstTime") == 70)
         {
             SceneManager.LoadScene(10);
         }
