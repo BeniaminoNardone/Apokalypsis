95f4b894a8e55f66c80b087d5f9d6a7456cd4ff3
Enrico Madonna <enrico.madonna@outlook.com>
2024-02-28 12:29:32 +0100

    eminem spazzolino morte con espllosione
    muore troppo

--- GoofySoft Game/Assets/Player/PlayerHealth.cs
+++ GoofySoft Game/Assets/Player/PlayerHealth.cs
@@ -6,7 +6,7 @@ public class PlayerHealth : MonoBehaviour
 {
     public int health;
     public int maxHealth = 3;
-
+    [SerializeField] private ParticleSystem particelleMortePlayer = default;
     public PlayerController playerController;
     public SpriteRenderer playerSr;
     // Start is called before the first frame update
@@ -15,14 +15,26 @@ public class PlayerHealth : MonoBehaviour
         health = maxHealth;
     }
 
-    // Update is called once per frame
     public void TakeDamage(int amount)
     {
         health -= amount;
-        if(health<= 0) {
+        if (health <= 0)
+        {
+            // Disattiva il collider
+            Collider2D collider = GetComponent<Collider2D>();
+            if (collider != null)
+            {
+                collider.enabled = false;
+            }
+
             playerSr.enabled = false;
             playerController.enabled = false;
+            particelleMortePlayer.transform.position = transform.position; // Imposta la posizione delle particelle sulla posizione del giocatore
+            particelleMortePlayer.Play();
+            
+            //Destroy(gameObject); // Distrugge l'oggetto del giocatore solo se è ancora attivo
         }
-        
     }
+
+
 }
