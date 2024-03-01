629076202495b387f5ebcb6ebb1f4f9a8965ed2f
SkyRed003 <francescopalumbo574@gmail.com>
2024-03-01 12:26:38 +0100

    hi-score

--- GoofySoft Game/Assets/ScoreManager.cs
+++ GoofySoft Game/Assets/ScoreManager.cs
@@ -6,18 +6,32 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     public Text scoreText;
+    public Text hiScoreText;
     public static int scoreCount;
+    public static int hiScoreCount;
+    
     // Start is called before the first frame update
     void Start()
     {
+        if (PlayerPrefs.HasKey("HighScore"))
+         {
+            hiScoreCount = PlayerPrefs.GetInt("HighScore");
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(scoreCount > hiScoreCount) 
+        {
+
+            hiScoreCount = scoreCount;
+            PlayerPrefs.SetInt("HighScore", hiScoreCount);
+        }
        
-         scoreText.text = "Score: " + Mathf.Round(scoreCount);
+         scoreText.text = "Score: " + scoreCount;
+         hiScoreText.text = "Hi-Score: " + hiScoreCount;
         
     }
 }
